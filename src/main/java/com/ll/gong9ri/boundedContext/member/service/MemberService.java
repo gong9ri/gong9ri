1f3763e5889940b5be35dfc9886264041a1233de
Youngwoo Choi <yw823x@daum.net>
2023-06-26 14:12:05 +0900

    Feat: Store Member

--- src/main/java/com/ll/gong9ri/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/gong9ri/boundedContext/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.ll.gong9ri.boundedContext.member.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,8 +20,10 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true) // 아래 메서드들이 전부 readonly 라는 것을 명시, 나중을 위해
 public class MemberService {
 	private final PasswordEncoder passwordEncoder;
-
 	private final MemberRepository memberRepository;
+
+	@Value("${custom.store.storeNamePrefix}")
+	private String storeNamePrefix;
 
 	public Optional<Member> findByUsername(String username) {
 		return memberRepository.findByUsername(username);
@@ -31,13 +34,11 @@ public class MemberService {
 		return join(ProviderTypeCode.GONG9RI, username, password);
 	}
 
-	// 내부 처리함수, 일반회원가입, 소셜로그인을 통한 회원가입(최초 로그인 시 한번만 발생)에서 이 함수를 사용함
 	private RsData<Member> join(ProviderTypeCode providerTypeCode, String username, String password) {
 		if (findByUsername(username).isPresent()) {
 			return RsData.of("F-1", "해당 아이디(%s)는 이미 사용중입니다.".formatted(username));
 		}
 
-		// 소셜 로그인을 통한 회원가입에서는 비번이 없다.
 		if (StringUtils.hasText(password))
 			password = passwordEncoder.encode(password);
 
@@ -53,7 +54,6 @@ public class MemberService {
 		return RsData.of("S-1", "회원가입이 완료되었습니다.", member);
 	}
 
-	// 소셜 로그인(카카오, 구글, 네이버) 로그인이 될 때 마다 실행되는 함수
 	@Transactional
 	public RsData<Member> whenSocialLogin(ProviderTypeCode providerTypeCode, String username) {
 		Optional<Member> opMember = findByUsername(username);
@@ -61,5 +61,27 @@ public class MemberService {
 		return opMember.map(member -> RsData.of("S-2", "로그인 되었습니다.", member))
 			.orElseGet(() -> join(providerTypeCode, username, ""));
 
+	}
+
+	@Transactional
+	public RsData<Member> storeJoin(String storeName, String password) {
+		storeName = storeNamePrefix + storeName;
+		if (findByUsername(storeName).isPresent()) {
+			return RsData.of("F-1", "해당 아이디(%s)는 이미 사용중입니다.".formatted(storeName));
+		}
+
+		if (StringUtils.hasText(password))
+			password = passwordEncoder.encode(password);
+
+		Member member = Member
+			.builder()
+			.providerTypeCode(ProviderTypeCode.GONG9RI)
+			.username(storeName)
+			.password(password)
+			.build();
+
+		memberRepository.save(member);
+
+		return RsData.of("S-1", "회원가입이 완료되었습니다.", member);
 	}
 }
