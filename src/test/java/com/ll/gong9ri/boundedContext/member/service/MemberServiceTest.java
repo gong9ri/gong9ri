912b927828392ef0381e5419555efcb05a1692bc
Youngwoo Choi <yw823x@daum.net>
2023-06-26 14:12:05 +0900

    Feat: Store Member

---
+++ src/test/java/com/ll/gong9ri/boundedContext/member/service/MemberServiceTest.java
@@ -0,0 +1,38 @@
+package com.ll.gong9ri.boundedContext.member.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ll.gong9ri.base.rsData.RsData;
+import com.ll.gong9ri.boundedContext.member.entity.Member;
+
+@SpringBootTest
+class MemberServiceTest {
+	@Autowired
+	private MemberService memberService;
+
+	@Test
+	@DisplayName("default member join test")
+	void memberJoinTest() {
+		final String username = "dasdasdasd";
+		RsData<Member> rsMember = memberService.join(username, username + username);
+
+		assertThat(rsMember.isSuccess()).isTrue();
+		assertThat(rsMember.getData().getUsername()).isEqualTo(username);
+	}
+
+	@Test
+	@DisplayName("store member join test")
+	void storeJoinTest() {
+		final String username = "asddd";
+		RsData<Member> rsMember = memberService.storeJoin(username, username + username);
+
+		assertThat(rsMember.isSuccess()).isTrue();
+		assertThat(rsMember.getData().getUsername()).isNotEqualTo(username);
+		assertThat(rsMember.getData().getUsername()).contains("GONG9");
+	}
+}
