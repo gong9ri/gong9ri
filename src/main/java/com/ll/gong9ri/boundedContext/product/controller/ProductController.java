0576675e0efb846dc9c406ea50141aa54cbe7eab
havegrit <shin_jaean@naver.com>
2023-06-28 07:42:01 +0900

    Fix: merge conflict

--- src/main/java/com/ll/gong9ri/boundedContext/product/controller/ProductController.java
+++ src/main/java/com/ll/gong9ri/boundedContext/product/controller/ProductController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 //TODO: hasAuthority('store') 추가
 @Controller
 @RequestMapping("/product")
--- src/main/java/com/ll/gong9ri/boundedContext/product/repository/ProductRepository.java
+++ src/main/java/com/ll/gong9ri/boundedContext/product/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.ll.gong9ri.boundedContext.product.repository;
 
 import com.ll.gong9ri.boundedContext.product.entity.Product;
+import com.ll.gong9ri.boundedContext.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<List<Product>> findByStore(Store store);
 }
