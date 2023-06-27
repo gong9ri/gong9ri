0331942af2e82fbf21c5b33eb91719e35155d134
havegrit <shin_jaean@naver.com>
2023-06-28 07:42:01 +0900

    Fix: merge conflict

--- src/main/java/com/ll/gong9ri/boundedContext/product/entity/ProductOption.java
+++ src/main/java/com/ll/gong9ri/boundedContext/product/entity/ProductOption.java
@@ -4,7 +4,6 @@ import com.ll.gong9ri.base.baseEntity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
