0181964c6ba2f9280d133a1539572339463b177f
havegrit <shin_jaean@naver.com>
2023-06-29 22:10:23 +0900

    test: use ProductDTO array

--- src/test/java/com/ll/gong9ri/boundedContext/product/service/ProductServiceTest.java
+++ src/test/java/com/ll/gong9ri/boundedContext/product/service/ProductServiceTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -110,89 +111,5 @@ class ProductServiceTest {
         assertThat(addOptionRs.isSuccess()).isTrue();
         assertThat(productRs.getData().getName()).isEqualTo(productDTO.getName());
         assertThat(productRs.getData().getProductOptions()).hasSize(colorOptionList.size());
-    }
-
-    @Test
-    @DisplayName("search for all products")
-    void getAllProducts() {
-        List<String> colorOptionList = new ArrayList<>();
-        colorOptionList.add("블랙");
-        colorOptionList.add("화이트");
-        colorOptionList.add("아이보리");
-        colorOptionList.add("네이비");
-
-        ProductDTO productDTO = ProductDTO.builder()
-                .name("버튼 카라 반팔 니트")
-                .description("여름에도 시원하게 입을 수 있는 반팔 니트 입니다.")
-                .price(43000)
-                .maxPurchaseNum(2)
-                .build();
-
-        ProductOptionDTO productOptionDTO = ProductOptionDTO.builder()
-                .optionOneName("색상")
-                .optionOneDetails(colorOptionList)
-                .build();
-
-        RsData<Product> productRs = productService.registerProduct(productDTO);
-        productService.addOptionDetails(productRs.getData(), productOptionDTO);
-
-        RsData<List<Product>> getAllProductsRs = productService.getAllProducts();
-
-        assertThat(getAllProductsRs.isSuccess()).isTrue();
-        assertThat(getAllProductsRs.getData()).isNotEmpty();
-    }
-
-    @Test
-    @DisplayName("search all products")
-    void showAllProductsTest() {
-        ProductDTO product1 = ProductDTO.builder()
-                .name("반팔 티셔츠")
-                .price(15000)
-                .build();
-        ProductDTO product2 = ProductDTO.builder()
-                .name("긴팔 티셔츠")
-                .price(20000)
-                .build();
-        ProductDTO product3 = ProductDTO.builder()
-                .name("반바지")
-                .price(20000)
-                .build();
-        productService.registerProduct(product1);
-        productService.registerProduct(product2);
-        productService.registerProduct(product3);
-
-        RsData<List<Product>> getAllProductsRs = productService.getAllProducts();
-
-        assertThat(getAllProductsRs.isSuccess()).isTrue();
-        assertThat(getAllProductsRs.getData()).hasSize(3);
-    }
-
-    @Test
-    @DisplayName("search product test")
-    void searchProductTest() {
-        ProductDTO product1 = ProductDTO.builder()
-                .name("반팔 티셔츠")
-                .price(15000)
-                .build();
-        ProductDTO product2 = ProductDTO.builder()
-                .name("긴팔 티셔츠")
-                .price(20000)
-                .build();
-        ProductDTO product3 = ProductDTO.builder()
-                .name("반바지")
-                .price(20000)
-                .build();
-        productService.registerProduct(product1);
-        productService.registerProduct(product2);
-        productService.registerProduct(product3);
-
-        SearchDTO searchDTO = SearchDTO.builder()
-                .keyword("티셔츠")
-                .build();
-
-        RsData<List<Product>> searchRs = productService.search(searchDTO);
-
-        assertThat(searchRs.isSuccess()).isTrue();
-        assertThat(searchRs.getData()).hasSize(2);
     }
 }
