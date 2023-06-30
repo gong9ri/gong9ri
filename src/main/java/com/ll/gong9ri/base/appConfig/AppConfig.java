f0e87e6fa0507e397767ebfb4f1a627a35791f3c
KimWonhee626 <kwh7739@gmail.com>
2023-06-30 15:17:06 +0900

    fix: 충돌 수정

--- src/main/java/com/ll/gong9ri/base/appConfig/AppConfig.java
+++ src/main/java/com/ll/gong9ri/base/appConfig/AppConfig.java
@@ -1,6 +1,8 @@
 package com.ll.gong9ri.base.appConfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
@@ -8,10 +10,26 @@ import lombok.Getter;
 @Configuration
 public class AppConfig {
 	@Getter
+	private static ApplicationContext context;
+
+	@Autowired
+	public void setContext(ApplicationContext context) {
+		AppConfig.context = context;
+	}
+
+	@Getter
 	private static String defaultImageUploadURL;
 
 	@Value("${custom.image.upload-dir}")
-	public void setLikeablePersonFromMax(String defaultImageUploadURL) {
+	public void setDefaultImageUploadURL(String defaultImageUploadURL) {
 		AppConfig.defaultImageUploadURL = defaultImageUploadURL;
+	}
+
+	@Getter
+	private static String baseUrl;
+
+	@Value("${custom.site.baseUrl}")
+	public void setBaseUrl(String baseUrl) {
+		AppConfig.baseUrl = baseUrl;
 	}
 }
