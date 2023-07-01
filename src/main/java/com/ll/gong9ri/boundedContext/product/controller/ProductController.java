7f59e891cd023da082a9084d229d02106836333e
havegrit <shin_jaean@naver.com>
2023-07-01 23:18:13 +0900

    feat: add action method about product details

--- src/main/java/com/ll/gong9ri/boundedContext/product/controller/ProductController.java
+++ src/main/java/com/ll/gong9ri/boundedContext/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ll.gong9ri.boundedContext.product.controller;
 
 import com.ll.gong9ri.base.rq.Rq;
 import com.ll.gong9ri.base.rsData.RsData;
+import com.ll.gong9ri.boundedContext.product.dto.DetailDTO;
 import com.ll.gong9ri.boundedContext.product.dto.ProductDTO;
 import com.ll.gong9ri.boundedContext.product.dto.ProductOptionDTO;
 import com.ll.gong9ri.boundedContext.product.dto.SearchDTO;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 //TODO: hasAuthority('store') 추가
 @Controller
@@ -24,6 +26,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
     private static final String PRODUCTS = "products";
+    private static final String PRODUCT = "product";
     private final ProductService productService;
     private final Rq rq;
 
@@ -41,9 +44,9 @@ public class ProductController {
         if (productRs.isFail())
             return rq.historyBack(productRs);
 
-        session.setAttribute("product", productRs.getData().toDTO());
+        session.setAttribute(PRODUCT, productRs.getData().toDTO());
 
-        return rq.redirectWithMsg("/product/option", productRs.getMsg());
+        return rq.redirectWithMsg("/product/discount", productRs);
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -56,7 +59,7 @@ public class ProductController {
     @PostMapping("/option")
     public String addProductOptions(ProductOptionDTO productOptionDTO) {
         HttpSession session = rq.getSession();
-        Product product = ((ProductDTO) session.getAttribute("product")).toEntity();
+        Product product = ((ProductDTO) session.getAttribute(PRODUCT)).toEntity();
 
         RsData<Product> productRs = productService.addOptionDetails(product, productOptionDTO);
         if (productRs.isFail()) {
@@ -99,4 +102,20 @@ public class ProductController {
 
         model.addAttribute(PRODUCTS, productDTOList);
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/details")
+    public String showDetails(Model model, DetailDTO detailDTO) {
+        Optional<Product> optionalProduct = productService.getProducts(detailDTO.getProductId());
+
+        if (optionalProduct.isEmpty()) {
+            return rq.historyBack("등록된 상품이 존재하지 않습니다.");
+        }
+
+        ProductDTO productDTO = optionalProduct.get().toDTO();
+        model.addAttribute(PRODUCT, productDTO);
+
+        return "product/productDetails";
+    }
+
 }
