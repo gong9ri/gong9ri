1bf699fcb65313458755178cf94f51c30f096224
havegrit <shin_jaean@naver.com>
2023-07-03 14:24:52 +0900

    fix: revert merge commit

--- src/main/java/com/ll/gong9ri/boundedContext/product/controller/ProductController.java
+++ src/main/java/com/ll/gong9ri/boundedContext/product/controller/ProductController.java
@@ -117,9 +117,9 @@ public class ProductController {
         }
 
         ProductDTO productDTO = ProductDTO.toDTO(optionalProduct.get());
+
         model.addAttribute(PRODUCT, productDTO);
 
         return "product/productDetails";
     }
-
 }
