5bb59845497f85db7eca30d0e93bbbe86535750b
havegrit <shin_jaean@naver.com>
2023-07-06 11:19:57 +0900

    test: fix tests

--- src/test/java/com/ll/gong9ri/boundedContext/product/service/ProductServiceTest.java
+++ src/test/java/com/ll/gong9ri/boundedContext/product/service/ProductServiceTest.java
@@ -1,12 +1,9 @@
 package com.ll.gong9ri.boundedContext.product.service;
 
 import com.ll.gong9ri.base.rsData.RsData;
-import com.ll.gong9ri.boundedContext.product.dto.ProductDTO;
-import com.ll.gong9ri.boundedContext.product.dto.ProductOptionDTO;
-import com.ll.gong9ri.boundedContext.product.dto.SearchDTO;
+import com.ll.gong9ri.boundedContext.product.dto.*;
 import com.ll.gong9ri.boundedContext.product.entity.Product;
 import com.ll.gong9ri.boundedContext.product.entity.ProductDiscount;
-import com.ll.gong9ri.boundedContext.product.repository.ProductDiscountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -19,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,7 +28,7 @@ class ProductServiceTest {
     @Autowired
     private ProductService productService;
     @Autowired
-    private ProductDiscountRepository productDiscountRepository;
+    private ProductDiscountService productDiscountService;
 
     @Test
     @DisplayName("product registration test")
@@ -40,19 +38,18 @@ class ProductServiceTest {
         List<Integer> discountRateList = new ArrayList<>();
         discountRateList.add(10);
 
-        ProductDTO productDTO = ProductDTO.builder()
+
+        ProductRegisterDTO registerDTO = ProductRegisterDTO.builder()
                 .name("버튼 카라 반팔 니트")
                 .description("여름에도 시원하게 입을 수 있는 반팔 니트 입니다.")
                 .price(43000)
                 .maxPurchaseNum(2)
-                .headCounts(headCountList)
-                .salePrices(discountRateList)
                 .build();
 
-        RsData<Product> productRs = productService.registerProduct(productDTO);
+        RsData<Product> productRs = productService.registerProduct(registerDTO);
 
         assertThat(productRs.isSuccess()).isTrue();
-        assertThat(productRs.getData().getName()).isEqualTo(productDTO.getName());
+        assertThat(productRs.getData().getName()).isEqualTo(registerDTO.getName());
         assertThat(productRs.getData().getProductOptions()).isEmpty();
     }
 
@@ -71,30 +68,44 @@ class ProductServiceTest {
         sizeOptionList.add("l");
         sizeOptionList.add("xl");
 
+        List<ProductOptionNameDTO> optionNameDTOList = colorOptionList.stream()
+                .flatMap(color -> sizeOptionList.stream()
+                        .map(size -> ProductOptionNameDTO.builder()
+                                .optionOneName(color)
+                                .optionTwoName(size)
+                                .build()))
+                .toList();
 
         List<Integer> headCountList = new ArrayList<>();
         headCountList.add(10);
-        List<Integer> discountRateList = new ArrayList<>();
-        discountRateList.add(10);
+        List<Integer> discountPriceList = new ArrayList<>();
+        discountPriceList.add(10);
 
-        ProductDTO productDTO = ProductDTO.builder()
-                .name("버튼 카라 반팔 니트")
-                .description("여름에도 시원하게 입을 수 있는 반팔 니트 입니다.")
-                .price(43000)
-                .maxPurchaseNum(2)
-                .headCounts(headCountList)
-                .salePrices(discountRateList)
-                .build();
+        List<ProductDiscountDTO> discountDTOList = headCountList.stream()
+                .flatMap(headCount -> discountPriceList.stream()
+                        .map(discount -> ProductDiscountDTO.builder()
+                                .headCount(headCount)
+                                .salePrice(discount)
+                                .build()))
+                .toList();
+
+        ProductRegisterDTO productDTO =
+                ProductRegisterDTO.builder()
+                        .name("버튼 카라 반팔 니트")
+                        .description("여름에도 시원하게 입을 수 있는 반팔 니트 입니다.")
+                        .price(43000)
+                        .maxPurchaseNum(2)
+                        .build();
 
         ProductOptionDTO productOptionDTO = ProductOptionDTO.builder()
-                .optionOneName("색상")
-                .optionTwoName("사이즈")
-                .optionOneDetails(colorOptionList)
-                .optionTwoDetails(sizeOptionList)
+                .optionOne("색상")
+                .optionTwo("사이즈")
+                .optionNames(optionNameDTOList)
                 .build();
 
         RsData<Product> productRs = productService.registerProduct(productDTO);
-        RsData<Product> addOptionRs = productService.addOptionDetails(productRs.getData(), productOptionDTO);
+        discountDTOList.forEach(e -> productDiscountService.create(productRs.getData(), e));
+        RsData<Product> addOptionRs = productService.addOptions(productRs.getData().getId(), productOptionDTO);
 
         assertThat(productRs.isSuccess()).isTrue();
         assertThat(addOptionRs.isSuccess()).isTrue();
@@ -111,31 +122,35 @@ class ProductServiceTest {
         colorOptionList.add("아이보리");
         colorOptionList.add("네이비");
 
+        List<ProductOptionNameDTO> optionNameDTOList = colorOptionList.stream()
+                .map(e -> ProductOptionNameDTO.builder()
+                        .optionTwoName(e)
+                        .build())
+                .toList();
+
         List<Integer> headCountList = new ArrayList<>();
         headCountList.add(10);
         List<Integer> discountRateList = new ArrayList<>();
         discountRateList.add(10);
 
-        ProductDTO productDTO = ProductDTO.builder()
+        ProductRegisterDTO registerDTO = ProductRegisterDTO.builder()
                 .name("버튼 카라 반팔 니트")
                 .description("여름에도 시원하게 입을 수 있는 반팔 니트 입니다.")
                 .price(43000)
                 .maxPurchaseNum(2)
-                .headCounts(headCountList)
-                .salePrices(discountRateList)
                 .build();
 
         ProductOptionDTO productOptionDTO = ProductOptionDTO.builder()
-                .optionOneName("색상")
-                .optionOneDetails(colorOptionList)
+                .optionOne("색상")
+                .optionNames(optionNameDTOList)
                 .build();
 
-        RsData<Product> productRs = productService.registerProduct(productDTO);
-        RsData<Product> addOptionRs = productService.addOptionDetails(productRs.getData(), productOptionDTO);
+        RsData<Product> productRs = productService.registerProduct(registerDTO);
+        RsData<Product> addOptionRs = productService.addOptions(productRs.getData().getId(), productOptionDTO);
 
         assertThat(productRs.isSuccess()).isTrue();
         assertThat(addOptionRs.isSuccess()).isTrue();
-        assertThat(productRs.getData().getName()).isEqualTo(productDTO.getName());
+        assertThat(productRs.getData().getName()).isEqualTo(registerDTO.getName());
         assertThat(productRs.getData().getProductOptions()).hasSize(colorOptionList.size());
     }
 
@@ -148,27 +163,31 @@ class ProductServiceTest {
         colorOptionList.add("아이보리");
         colorOptionList.add("네이비");
 
+        List<ProductOptionNameDTO> optionNameDTOList = colorOptionList.stream()
+                .map(e -> ProductOptionNameDTO.builder()
+                        .optionTwoName(e)
+                        .build())
+                .toList();
+
         List<Integer> headCountList = new ArrayList<>();
         headCountList.add(10);
         List<Integer> discountRateList = new ArrayList<>();
         discountRateList.add(10);
 
-        ProductDTO productDTO = ProductDTO.builder()
+        ProductRegisterDTO productDTO = ProductRegisterDTO.builder()
                 .name("버튼 카라 반팔 니트")
                 .description("여름에도 시원하게 입을 수 있는 반팔 니트 입니다.")
                 .price(43000)
                 .maxPurchaseNum(2)
-                .headCounts(headCountList)
-                .salePrices(discountRateList)
                 .build();
 
         ProductOptionDTO productOptionDTO = ProductOptionDTO.builder()
-                .optionOneName("색상")
-                .optionOneDetails(colorOptionList)
+                .optionOne("색상")
+                .optionNames(optionNameDTOList)
                 .build();
 
         RsData<Product> productRs = productService.registerProduct(productDTO);
-        productService.addOptionDetails(productRs.getData(), productOptionDTO);
+        productService.addOptions(productRs.getData().getId(), productOptionDTO);
 
         RsData<List<Product>> getAllProductsRs = productService.getAllProducts();
 
@@ -184,33 +203,27 @@ class ProductServiceTest {
         List<Integer> discountRateList = new ArrayList<>();
         discountRateList.add(10);
 
-        ProductDTO[] products = {
-                ProductDTO.builder()
+        ProductRegisterDTO[] productRegisterDTOs = {
+                ProductRegisterDTO.builder()
                         .name("반팔 티셔츠")
                         .price(15000)
-                        .headCounts(headCountList)
-                        .salePrices(discountRateList)
                         .build(),
-                ProductDTO.builder()
+                ProductRegisterDTO.builder()
                         .name("긴팔 티셔츠")
                         .price(20000)
-                        .headCounts(headCountList)
-                        .salePrices(discountRateList)
                         .build(),
-                ProductDTO.builder()
+                ProductRegisterDTO.builder()
                         .name("반바지")
                         .price(20000)
-                        .headCounts(headCountList)
-                        .salePrices(discountRateList)
                         .build()
         };
 
-        Arrays.stream(products).forEach(productService::registerProduct);
+        Arrays.stream(productRegisterDTOs).forEach(productService::registerProduct);
 
         RsData<List<Product>> getAllProductsRs = productService.getAllProducts();
 
         assertThat(getAllProductsRs.isSuccess()).isTrue();
-        assertThat(getAllProductsRs.getData()).hasSize(products.length);
+        assertThat(getAllProductsRs.getData()).hasSize(productRegisterDTOs.length);
     }
 
     @Test
@@ -221,28 +234,22 @@ class ProductServiceTest {
         List<Integer> discountRateList = new ArrayList<>();
         discountRateList.add(10);
 
-        ProductDTO[] products = {
-                ProductDTO.builder()
+        ProductRegisterDTO[] productRegisterDTOs = {
+                ProductRegisterDTO.builder()
                         .name("반팔 티셔츠")
                         .price(15000)
-                        .headCounts(headCountList)
-                        .salePrices(discountRateList)
                         .build(),
-                ProductDTO.builder()
+                ProductRegisterDTO.builder()
                         .name("긴팔 티셔츠")
                         .price(20000)
-                        .headCounts(headCountList)
-                        .salePrices(discountRateList)
                         .build(),
-                ProductDTO.builder()
+                ProductRegisterDTO.builder()
                         .name("반바지")
                         .price(20000)
-                        .headCounts(headCountList)
-                        .salePrices(discountRateList)
                         .build()
         };
 
-        Arrays.stream(products).forEach(productService::registerProduct);
+        Arrays.stream(productRegisterDTOs).forEach(productService::registerProduct);
 
         SearchDTO searchDTO = SearchDTO.builder()
                 .keyword("티셔츠")
@@ -255,22 +262,27 @@ class ProductServiceTest {
     }
 
     @Test
-    @DisplayName("save product's discountRate")
+    @DisplayName("save product's discount price")
     void discountRateSaveTest() {
-        List<ProductDiscount> productDiscounts = new ArrayList<>() {{
-            add(ProductDiscount.builder()
-                    .headCount(10)
-                    .salePrice(5)
-                    .build());
-        }};
-        RsData<List<ProductDiscount>> productRs = productService.saveProductDiscount(productDiscounts);
+        ProductRegisterDTO registerDTO = ProductRegisterDTO.builder()
+                .name("반팔 티셔츠")
+                .price(15000)
+                .build();
 
-        assertThat(productRs.isSuccess()).isTrue();
+        ProductDiscountDTO productDiscountDTO =
+                ProductDiscountDTO.builder()
+                        .headCount(10)
+                        .salePrice(5)
+                        .build();
 
-        List<ProductDiscount> productDiscountList = productRs.getData().stream()
-                .filter(e -> productDiscountRepository.findById(e.getId()).get().equals(e))
-                .toList();
+        RsData<Product> productRs = productService.registerProduct(registerDTO);
 
-        assertThat(productDiscountList).hasSameSizeAs(productDiscounts);
+        RsData<ProductDiscount> productDiscountRs = productDiscountService.create(productRs.getData(), productDiscountDTO);
+        assertThat(productDiscountRs.isSuccess()).isTrue();
+
+        Optional<ProductDiscount> oProductDiscount = productDiscountService.findById(productDiscountRs.getData().getId());
+        assertThat(oProductDiscount)
+                .isPresent()
+                .contains(productDiscountRs.getData());
     }
 }
